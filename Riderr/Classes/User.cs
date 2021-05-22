f7b1f27df7f2e0f00bc23767e471b2e7e0eefa60
rot4ry <wojtekw233@gmail.com>
2021-05-22 10:50:56 +0200

    Added: Project files.

---
+++ Riderr/Classes/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riderr.Classes
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string SecondNeme { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        //photos?
+    }
+}
---
+++ Riderr/Controllers/MainController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Riderr.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riderr.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
