fa495930a4ed26d8d0f6ab9a03744f5e9357a966
rot4ry <wojtekw233@gmail.com>
2021-05-31 00:13:49 +0200

    Added: Database handler class, no code yet.

---
+++ Riderr/Classes/DBHandler/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riderr.Classes.DBHandler
+{
+    public class DbContext
+    {
+        //todo - context
+        //          users|  pairs|  messages
+        //create    +       +       +
+        //read      +       +       +
+        //update    +       +       -
+        //delete    +       +       -
+    }
+}
