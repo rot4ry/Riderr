b3f9ae760bce743bcc16298ac6a182e0c3933dd4
rot4ry <wojtekw233@gmail.com>
2021-05-31 10:50:05 +0200

    Update: working on User part of the database.

--- Riderr/Classes/DBHandler/DbContext.cs
+++ Riderr/Classes/DBHandler/DbContext.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,5 +17,75 @@ namespace Riderr.Classes.DBHandler
         //read      +       +       +
         //update    +       +       -
         //delete    +       +       -
+
+
+        private readonly IGridFSBucket _gridFS;
+        private static readonly string _connectionString = "mongodb://localhost";
+        private MongoClient _client { get; set; }
+        private IMongoDatabase _database { get; set; }
+
+        public DbContext()
+        {
+            _client = new MongoClient(_connectionString);
+            _database = _client.GetDatabase("riderr");
+            _gridFS = new GridFSBucket(_database);
+        }
+        
+        //Collections
+        public IMongoCollection<User> Users
+        {
+            get => _database.GetCollection<User>("users");
+        }
+
+        //public IMongoCollection<Message> Messages
+        //{
+        //    get => _database.GetCollection<Message>("messages");
+        //}
+
+        
+        
+        //Users
+        private async Task<User> GetUser(string _id)
+        {
+            return await Users.Find(new BsonDocument("_id", new ObjectId(_id))).FirstOrDefaultAsync();
+        }
+        
+        public async Task<IEnumerable<User>> GetUsers(BsonDocument filter)
+        {
+            if(filter is null)
+            {
+                filter = new BsonDocument();
+            }
+            return await Users.Find(filter).ToListAsync();
+        }
+
+        public async Task AddUser(User user)
+        {
+            await Users.InsertOneAsync(user);
+        }
+
+        //UpdateUser
+        //RemoveUser
+
+        //Images
+        public async Task StoreImage(string _id, Stream imageStream, string imageName)
+        {
+            User user = await GetUser(_id);
+            if (user.HasImage())
+            {
+                await _gridFS.DeleteAsync(new ObjectId(user.ImageId));
+            }
+            var imageId = await _gridFS.UploadFromStreamAsync(imageName, imageStream);
+            user.ImageId = imageId.ToString();
+
+            var filter = Builders<User>.Filter.Eq("_id", new ObjectId(user.UserId));
+            var update = Builders<User>.Update.Set("ImageId", user.ImageId);
+            await Users.UpdateOneAsync(filter, update);
+        }
+
+        public async Task<byte[]> GetUserImage(string _id)
+        {
+            return await _gridFS.DownloadAsBytesAsync(new ObjectId(_id));
+        }
     }
 }
--- Riderr/Classes/User.cs
+++ Riderr/Classes/User.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson;
 
 namespace Riderr.Classes
 {
     public class User
     {
+        public string UserId { get; set; }
         public string FirstName { get; set; }
-        public string SecondNeme { get; set; }
+        public string SecondName { get; set; }
         public string Phone { get; set; }
         public string Mail { get; set; }
-        //photos?
+        //public string[] Images { get; set; }
+        //public bool HasImage()
+        //{
+        //    if (Images.Length > 0 || !(Images is null))
+        //    {
+        //        return true;
+        //    }
+        //    else return false;
+        //}
+        public string ImageId { get; set; }
+        public bool HasImage()
+        {
+            if (ImageId is null) return false;
+            else return true;
+        }
     }
 }
